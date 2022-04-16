517e3ebaaa370d760b92cbc9db0ca8956f66db24
Luke Jones <lukejones@Lukes-iMac.local>
2022-04-16 14:15:37 -0400

    Improve overal UX design for website, and add additional contact modal.  Begin new concept for photos modal.

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/index.html");
-})
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
 
 app.listen(3000, function() {
-    console.log("Server is up and running on port 3000!");
-})
+	console.log('Server is up and running on port 3000!');
+});
