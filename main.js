18421b4ece3745c6717aed98acb49092aac2d78d
ulbrii <luke.jones6@yahoo.com>
2022-04-25 10:35:01 -0400

    Additional JavaScript and modal spacing
    Add social footers to modals via JavaScript.
    Adjust responsive sizing within the About and Discography modals.
    More comment text to help clean-up & organize HTML page.

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 // Band Members Array
-
 const bandMembers = [
 	{
 		name: 'lukejones',
@@ -32,4 +31,33 @@ for (let member of bandMembers) {
 	memberPhoto.addEventListener('mouseout', () =>
 		memberPhoto.setAttribute('src', `/public/images/${member.name}-rounded.png`)
 	);
+}
+
+
+// Adding social footer to all modals.
+const modalFooter = document.querySelectorAll('.modal-footer');
+
+for(let footer of modalFooter) {
+	const modalSocial = document.createElement('section');
+	modalSocial.classList.add('modal-social', 'me-auto');
+	modalSocial.innerHTML = `
+	<div class="justify-content-center container">
+		<a href="https://www.instagram.com/esbernsnare" target="_blank">
+		<i class="fa-brands fa-instagram fa-xl"></i>
+		</a>
+		<a href="https://www.facebook.com/esbernsnaremusic" target="_blank">
+		<i class="fa-brands fa-facebook fa-xl"></i>
+		</a>
+		<a href="mailto:esbernsnareband@gmail.com">
+		<i class="fa-solid fa-envelope fa-xl"></i>
+		</a>
+		<a href="https://esbernsnare.bandcamp.com/" target="_blank">
+		<i class="fa-brands fa-bandcamp fa-xl"></i>
+		</a>
+		<a href="https://open.spotify.com/artist/1VbRx9HJz7ZVbkp6TVy3eq" target="_blank">
+		<i class="fa-brands fa-spotify fa-xl"></i>
+		</a>
+	</div>
+	`;
+	footer.insertAdjacentElement('afterbegin', modalSocial);
 }
